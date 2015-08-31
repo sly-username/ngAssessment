a17cb4e9e6721380c667b01f5c562edf04304199
Sylvia <sylviaxh@gmail.com>
2015-08-31 04:54:46 -0700

    Removing uncessary dependencies.

--- public/components/guestbook/guestbook.controller.js
+++ public/components/guestbook/guestbook.controller.js
@@ -1,23 +1,20 @@
 'use strict';
 
 angular.module( 'starsApp.controllers' )
-  .controller( 'GuestbookController', [ '$scope', 'Book', 'Publish', '$location',function( $scope, Book, Publish, $location ) {
+  .controller( 'GuestbookController', [ '$scope', 'Book', 'Publish', function( $scope, Book, Publish ) {
 
     $scope.guestbook = Book.query();
 
     $scope.savePost = function savePost() {
-      var post = new Publish();
       Publish.save({}, { phone: $scope.telephone, message: $scope.message }).$promise
         .then( function ( response ) {
-          $scope.message = response.message;
-          $scope.phone = response.phone;
-          console.log( post );
-          $scope.guestbook.push( post );
+        $scope.message = response.message;
+        $scope.phone = response.phone;
       })
-        .catch( function ( status, data ) {
-          console.log( status );
-          console.log( data );
-        });
+      .catch( function ( status, data ) {
+        console.log( status );
+        console.log( data );
+      });
     }
 
   }]);
