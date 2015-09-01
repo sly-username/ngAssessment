4904854f3533da1874c448c2e590de80e03e59e7
Sylvia <sylviaxh@gmail.com>
2015-08-31 23:20:23 -0700

    omg so annoying

---
+++ public/components/states/states.controller.js
@@ -0,0 +1,25 @@
+'use strict';
+
+angular.module( 'statesApp.controllers' )
+  .controller( 'StatesController', [ '$scope', 'State', 'AbbrevList', function( $scope,  State ) {
+    var states = [],
+      partialList,
+      offsetArray = [ 0, 10, 20, 30, 40 ],
+      getBatch = function() {
+        offsetArray.forEach( function( index ) {
+          partialList = State.query({'offset': index});
+          states.push( partialList );
+
+        });
+      };
+
+    getBatch();
+
+    $scope.nameFilter = null;
+    $scope.states = states;
+    $scope.searchFilter = function ( state ) {
+      var keyword = new RegExp( $scope.nameFilter, 'i' );
+      return !$scope.nameFilter || keyword.test( state.name ) || keyword.test( state.abbreviation );
+    }
+
+  }]);
